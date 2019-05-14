fb2b2c03fe65d40836bf7932ee29dc0c640334ab
serj-minaev <miner_x@bk.ru>
2019-05-14 16:32:45 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let text = 'Hello World!';
+
+let root = document.getElementById('root');
+
+
+
+console.log(text);
+
+
+let Axe = {
+    race: 'Orc', 
+    cl: 'Warrior',
+    color: 'red'
+};
+
+
+
+root.innerHTML = text + (Axe.race);
