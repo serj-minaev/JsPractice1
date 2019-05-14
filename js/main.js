d9207bfc741656778c9f3e96d400ee8fb10b500c
serj-minaev <miner_x@bk.ru>
2019-05-14 17:21:51 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -14,14 +14,33 @@ let Axe = {
 
 
 // Start practice
-let mainList = {
-    Money,
-    ShopName
+let mainList;
+mainList = {
+    Money: '',
+    ShopName: '',
+    shopGoods: [],
+    employers: '',
+    open: true
+};
 
-}
+let country = {
+
+    name: "Швейцария",
+    languages: ["немецкий", "французский", "итальянский"],
+    capital:{
+
+        name: "Берн",
+        population: 126598
+    },
+    cities: [
+        { name: "Цюрих", population: 378884},
+        { name: "Женева", population: 188634},
+        { name: "Базель", population: 164937}
+    ]
+};
 
 Money = prompt("Ваш бюджет?", '100');
 ShopName = prompt("Название вашего магазина?", 'Маргаритка');
 
 
-console.log(mainList);
+console.log(country);
